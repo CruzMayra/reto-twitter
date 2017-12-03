812d5ebded6cb6d6cdb52d053302d9b6141c9669
CruzMayra <velazquez.leonora@gmail.com>
2017-12-03 12:09:37 -0600

    Creando función que valida campo vacío y no envio de tweet

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
-var btnSend = document.getElementById('new-tweet'); //.lastElementChild;
+var btnSend = document.getElementById('new-tweet'); //variable para obtener formulario (evento)
 
-var historial = document.getElementsByClassName('cuadro-3')[0];
+var historial = document.getElementsByClassName('cuadro-3')[0]; // variable para obtener la sección con el historial de tweets
 
 
-btnSend.onsubmit = function(event){
+btnSend.onsubmit = function(event){ // función para desencadenar evento
   event.preventDefault();
   var tweetText = event.target.getElementsByTagName('textarea')[0].value;
-  newHist(tweetText);
+  validation(tweetText);
 }
 
-var newHist = function(tweetText) {
+function validation(tweetText) { // función para la validación de caracteres
+  if(tweetText == '' || /^\s+$/.test(tweetText)) {
+    return false;
+  }else {
+    newHist(tweetText);
+  }
+}
+
+var newHist = function(tweetText) { // función para crear nuevo párrafo con tweet e ingresarlo en la sección de historial
   var newTweet = document.createElement('p');
   newTweet.textContent = tweetText;
   historial.appendChild(newTweet);
