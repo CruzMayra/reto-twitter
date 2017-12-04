e793c14b14a336a15a585d1104453346d604993b
CruzMayra <velazquez.leonora@gmail.com>
2017-12-04 00:45:17 -0600

    creación de funciones para las versiones 0.0.4 y 0.0.6

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ var tweetContainer = btnSend[0]; // variable para obtener la textarea
 var historial = document.getElementsByClassName('cuadro-3')[0]; // variable para obtener la sección con el historial de tweets
 var meter = document.getElementById('meter'); //meter es contador en inglés
 
-
 btnSend.onsubmit = function(event){ // función para desencadenar evento
   event.preventDefault();
   var tweetText = tweetContainer.value;
@@ -23,11 +22,15 @@ function validation(tweetText) { // función para la validación de caracteres
 
 var newHist = function(tweetText) { // función para crear nuevo párrafo con tweet e ingresarlo en la sección de historial
   var newTweet = document.createElement('p');
+  var timeSpan = document.createElement('span');
   newTweet.textContent = tweetText;
+  newTweet.dataset.tweeted = Date.now();
+  newTweet.appendChild(document.createElement('br'));
+  newTweet.appendChild(timeSpan);
   historial.appendChild(newTweet);
 }
 
-var count = function() { // función que cuenta el número de caracteres del tweet
+var count = function(e) { // función que cuenta el número de caracteres del tweet
   var tweetText = tweetContainer.value;
   var numType = tweetText.length;
   meterReal(numType);
@@ -57,3 +60,34 @@ var alertText = function(numType) { // función que da un color determinado seg�
      meter.classList.remove('red');
    }
 }
+
+var validKey = function(e){ // funcion que valida la tecla que pulsa el usuario
+  var key = e.keyCode;
+  if(key === 13) {
+    autoSize();
+  }
+}
+
+var autoSize = function() { // función que aumenta el tamaño del textarea al dar enter (/n)
+  tweetContainer.style.height = tweetContainer.scrollHeight + 'px';
+  //console.log('esto funciona');
+}
+
+tweetContainer.addEventListener('keyup',count);
+tweetContainer.addEventListener('keydown',count);
+tweetContainer.onkeyup = validKey;
+
+var updateTweets = function() {
+  var tweets = historial.getElementsByTagName('p');
+
+  for(var i = 0; i < tweets.length; i++) {
+    var tweeted = tweets[i].dataset.tweeted;
+    var haceCuanto = moment(parseInt(tweeted)).fromNow();
+    tweets[i].getElementsByTagName('span')[0].innerHTML = haceCuanto;
+  }
+}
+
+setInterval(updateTweets, 5000)
+
+historial.getElementsByTagName('p')[0].dataset.tweeted = Date.now()
+updateTweets()
